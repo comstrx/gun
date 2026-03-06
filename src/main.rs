f226cdfc65a6ae5d79f5053274b98813e213d673
Core Master <comstrx@gmail.com>
2026-03-06 02:59:19 +0200

    Done from Gun

--- src/main.rs
+++ src/main.rs
@@ -15,10 +15,10 @@ fn run () -> AppResult<()> {
 
     Manager::run_output("git", &["add", "."])?;
     Manager::run_output("git", &["commit", "-m", "Done from Gun"])?;
-    let output = Manager::run_output("git", &["push"])?;
 
-    print!("{}", String::from_utf8_lossy(&output.stdout));
-    eprint!("{}", String::from_utf8_lossy(&output.stderr));
+    Manager::run_live("git", &["push"])?;
+    // let output = Manager::run_output("git", &["push"])?;
+    // print!("{}", String::from_utf8_lossy(&output.stdout));
 
     Ok(())
 
